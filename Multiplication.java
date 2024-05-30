8402b399b88d30f31733151ff3ee6f5c6d60afd1
Saurabh Manral <saurabhmanral08@gmail.com>
2024-05-30 13:54:50 +0530

    Added some new funtionality in Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -7,6 +7,6 @@ public class Multiplication {
         System.out.println("Enter second number: ");
         int num2 = sc.nextInt();
 
-        System.out.println("The multiplication of two number is: " + (num1 * num2));
+        System.out.println("The multiplication of " + num1 + " and " + num2 + " is: " + (num1 * num2));
     }
 }
