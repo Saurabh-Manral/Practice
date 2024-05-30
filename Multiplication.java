7caa84891d14c37adb375ddefe3058ce03447497
Saurabh Manral <saurabhmanral08@gmail.com>
2024-05-30 13:04:30 +0530

    Added multiplication funtionality

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanenr;
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.println("The multiplication of two number is: " + (num1 * num2));
+    }
+}
